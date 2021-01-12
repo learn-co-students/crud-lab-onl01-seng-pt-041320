ebb67374ae356c53b7cb71a2d35320f10c7d8d52
OrkunSA <c.orkun.s@gmail.com>
2021-01-12 15:51:40 -0600

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import Review from './Review';
+import React, { Component } from "react";
+import Review from "./Review";
 
 class Reviews extends Component {
   render() {
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const resReviews = reviews.filter(
+      (review) => review.restaurantId === restaurantId
+    );
+    const reviewList = resReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />;
+    });
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>{reviewList}</ul>
+      </div>
     );
   }
-};
+}
 
 export default Reviews;
