f3ed9e74151814157454d57b171c83b7c28d8191
Dice-K39 <daisukehashimoto39@gmail.com>
2021-01-17 23:11:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import rootReducer from './reducers/manageRestaurant';
+import manageRestaurant from './reducers/manageRestaurant';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(rootReducer);
+const store = createStore(manageRestaurant);
 
 
 ReactDOM.render(
